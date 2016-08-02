84c874bbf84d403cefe5169f1f5b8ec2dacb98b0
Clemente Morales Fernandez <morales.clements@gmail.com>
2016-08-02 09:09:33 -0400

    LaBuena feature Add JDBC dependency and integration.

--- LaBuenaServices/src/main/java/mx/com/labuena/services/BikersEndpoint.java
+++ LaBuenaServices/src/main/java/mx/com/labuena/services/BikersEndpoint.java
@@ -10,9 +10,14 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.ServletException;
 
 import mx.com.labuena.services.responses.BikersResponse;
 import mx.com.labuena.services.tos.Biker;
@@ -37,12 +42,61 @@ public class BikersEndpoint {
     @ApiMethod(name = "getAll",
             httpMethod = ApiMethod.HttpMethod.GET)
     public BikersResponse getAll() {
-        List<Biker> bikers = new ArrayList<>();
-        Location location = new Location(new BigDecimal("47.6"), new BigDecimal("122.33"));
-        bikers.add(new Biker("Clemente", "morales.fernandez.clemente@gmail.com", "7347896184", location, 35));
-
-        Location location2 = new Location(new BigDecimal("47.6"), new BigDecimal("122.33"));
-        bikers.add(new Biker("William", "morales.fernandez.williams@gmail.com", "73478145184", location2, 35));
+        List<Biker> bikers = getBikers();
         return new BikersResponse(bikers);
     }
+
+
+    private List<Biker> getBikers() {
+        List<Biker> bikers = new ArrayList<>();
+        Connection conn = null;
+
+        try {
+            String url;
+            if (System.getProperty("com.google.appengine.runtime.version").startsWith("Google App Engine/")) {
+                url = System.getProperty("ae-cloudsql.cloudsql-database-url");
+                try {
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                } catch (ClassNotFoundException exception) {
+                    throw new ServletException("Error loading Google JDBC Driver", exception);
+                }
+            } else {
+                url = System.getProperty("ae-cloudsql.local-database-url");
+            }
+
+            String bikersQuery = "select email, phone, stock from biker";
+
+
+            conn = DriverManager.getConnection(url);
+            ResultSet rs = conn.prepareStatement(bikersQuery).executeQuery();
+            while (rs.next()) {
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                int stock = rs.getInt("stock");
+
+                Location lastLocation = new Location();
+                bikers.add(new Biker("Clemente", email, phone, lastLocation, stock));
+            }
+            rs.close();
+            conn.close();
+        } catch (ServletException e) {
+            e.printStackTrace();
+            Biker biker = new Biker();
+            biker.setName(e.getMessage());
+            bikers.add(biker);
+        } catch (SQLException e) {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            Biker biker = new Biker();
+            biker.setName(e.getMessage());
+            bikers.add(biker);
+        }
+        return bikers;
+    }
+
 }
