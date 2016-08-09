7ea6dd24d2288e294c2c411e9c9d2a1cfee58351
Clemente Morales Fernandez <morales.clements@gmail.com>
2016-08-08 21:12:47 -0400

    QuieroTortillas feature Add events to controls from requst tortillas view

--- QuieroTortillas/src/main/java/mx/com/labuena/tortillas/views/fragments/TortillasRequestorFragment.java
+++ QuieroTortillas/src/main/java/mx/com/labuena/tortillas/views/fragments/TortillasRequestorFragment.java
@@ -2,6 +2,7 @@ package mx.com.labuena.tortillas.views.fragments;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +19,17 @@ public class TortillasRequestorFragment extends BaseFragment {
 
     public static final String LOGIN_USER_KEY = "LoginUser";
 
-    User user;
+    public static final int DEFAULT_CONSUME = 3;
+
+    public static final int MAX_AMOUNT = 40;
+    public static final int MIN_AMOUNT = 1;
+
+
+    private User user;
+
+    private int tortillasAmount = DEFAULT_CONSUME;
+
+    private TextView tortillasAmontTextview;
 
     @Override
     protected int getLayoutId() {
@@ -50,7 +61,39 @@ public class TortillasRequestorFragment extends BaseFragment {
                 .centerCrop()
                 .into(userPhoto);
 
-        TextView nameTextView = (TextView)rootView.findViewById(R.id.welcomeTextView);
+        TextView nameTextView = (TextView) rootView.findViewById(R.id.welcomeTextView);
         nameTextView.setText(String.format("Welcome %s", user.getName()));
+
+        tortillasAmontTextview = (TextView) rootView.findViewById(R.id.amountTextview);
+
+        addEventsToControls(rootView);
+    }
+
+    private void addEventsToControls(View rootView) {
+        Button buttonMoreTortillas = (Button) rootView.findViewById(R.id.increaseAmountButton);
+        buttonMoreTortillas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tortillasAmount <= MAX_AMOUNT) {
+                    ++tortillasAmount;
+                    displayTortillasAmount();
+                }
+            }
+        });
+
+        Button buttonLessTortillas = (Button) rootView.findViewById(R.id.decreaseAmountButton);
+        buttonLessTortillas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tortillasAmount > MIN_AMOUNT) {
+                    --tortillasAmount;
+                    displayTortillasAmount();
+                }
+            }
+        });
+    }
+
+    private void displayTortillasAmount() {
+        tortillasAmontTextview.setText(String.format("%d", tortillasAmount));
     }
 }
