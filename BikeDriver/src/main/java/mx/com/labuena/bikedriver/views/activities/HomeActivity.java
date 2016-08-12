6dc41ab9afc28dc7981b950c645ad5c8b6417c0c
Clemente Morales Fernandez <morales.fernandez.clemente@gmail.com>
2016-08-12 14:49:36 -0400

    BikeDriver feature Add base activity

---
+++ BikeDriver/src/main/java/mx/com/labuena/bikedriver/views/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package mx.com.labuena.bikedriver.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import mx.com.labuena.bikedriver.R;
+
+/**
+ * Created by moracl6 on 8/12/2016.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
