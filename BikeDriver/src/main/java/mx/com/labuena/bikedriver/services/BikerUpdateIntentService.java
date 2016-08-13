202c56cc793ce94c4ceb19dc538fdb1ecddef74b
Clemente Morales Fernandez <morales.clements@gmail.com>
2016-08-13 16:19:53 -0400

    BikeDriver feature On biker authentication, update biker token in central cerver if required

--- BikeDriver/src/main/java/mx/com/labuena/bikedriver/services/BikerUpdateIntentService.java
+++ BikeDriver/src/main/java/mx/com/labuena/bikedriver/services/BikerUpdateIntentService.java
@@ -1,6 +1,21 @@
 package mx.com.labuena.bikedriver.services;
 
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import mx.com.labuena.bikedriver.models.BikeDriver;
+import mx.com.labuena.bikedriver.models.PreferencesRepository;
+import mx.com.labuena.bikedriver.setup.LaBuenaApplication;
+import mx.com.labuena.bikedriver.setup.LaBuenaModules;
+import mx.com.labuena.services.bikers.Bikers;
+import mx.com.labuena.services.bikers.model.Biker;
 
 /**
  * Created by moracl6 on 8/12/2016.
@@ -10,12 +25,41 @@ public class BikerUpdateIntentService extends EndpointConsumerBaseService {
     private static final String TAG = BikerUpdateIntentService.class.getSimpleName();
     public static final String BIKER_DATA_EXTRA = "BikerExtraData";
 
+    @Inject
+    PreferencesRepository sharedPreferencesRepository;
+
     public BikerUpdateIntentService() {
         super(TAG);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        LaBuenaModules modules = LaBuenaApplication.getObjectGraph(this
+                .getApplicationContext());
+        modules.inject(this);
 
+        BikeDriver bikeDriver = intent.getParcelableExtra(BIKER_DATA_EXTRA);
+        String rootUrl = getRootUrl();
+        Bikers.Builder builder = new Bikers.Builder(AndroidHttp.newCompatibleTransport(),
+                new AndroidJsonFactory(), null).setApplicationName(getApplicationName(this))
+                .setRootUrl(rootUrl);
+
+        Bikers bikersService = builder.build();
+        try {
+            bikersService.updateToken(buildBiker(bikeDriver)).execute();
+            sharedPreferencesRepository.save(BikerInstanceIdService.TOKEN_IN_SERVER_KEY, true);
+            Log.d(TAG, "Bike driver token successfully updated.");
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage(), e);
+        }
     }
+
+    private Biker buildBiker(BikeDriver bikeDriver) {
+        Biker biker = new Biker();
+        biker.setEmail(bikeDriver.getEmail());
+        biker.setGcmToken(bikeDriver.getFcmToken());
+        return biker;
+    }
+
+
 }
--- BikeDriver/src/main/java/mx/com/labuena/bikedriver/setup/LaBuenaModules.java
+++ BikeDriver/src/main/java/mx/com/labuena/bikedriver/setup/LaBuenaModules.java
@@ -4,6 +4,7 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import mx.com.labuena.bikedriver.services.BikerInstanceIdService;
+import mx.com.labuena.bikedriver.services.BikerUpdateIntentService;
 import mx.com.labuena.bikedriver.views.activities.HomeActivity;
 import mx.com.labuena.bikedriver.views.fragments.LoginFragment;
 
@@ -18,4 +19,6 @@ public interface LaBuenaModules {
     void inject(BikerInstanceIdService bikerInstanceIdService);
 
     void inject(LoginFragment loginFragment);
+
+    void inject(BikerUpdateIntentService bikerUpdateIntentService);
 }
