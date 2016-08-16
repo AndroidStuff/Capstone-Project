5456f284618c0d5937b212f4ebd3214fe3e63aaf
Clemente Morales Fernandez <morales.clements@gmail.com>
2016-08-15 21:32:49 -0400

    LaBuena feature Include la buena branches in project

---
+++ LaBuena/src/main/java/mx/com/labuena/branch/views/activities/HomeActivity.java
@@ -0,0 +1,17 @@
+package mx.com.labuena.branch.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import mx.com.labuena.branch.R;
+
+/**
+ * Created by clerks on 8/15/16.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
